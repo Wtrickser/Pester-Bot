27714ae55c05cad0fb179f27ebf1dba14622f415
Joseph <JSPenland97@gmai.com>
2021-05-16 21:23:55 -0400

    frontend

---
+++ client/src/components/Info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Info = () => (
+	<div className="card">
+		<div className="card-image" style={{heigth: "150px"}}>
+          <span className="card-title">Welcome to Pester-bot!</span>
+        </div>
+        <div className="card-content">
+          <p>
+          This is a simple chatbot powered by Cleverbot API. You can asking it anything and it will do it's best to hold a conversion.
+          You can also tell the bot to change the message box color by type "set style A" or "set style B"
+          </p>
+        </div>
+	</div>
+);
+
+export default Info;
