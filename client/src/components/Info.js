b2d92af2397e05e85f62e0e81f1e9393efc30841
Joseph <JSPenland97@gmai.com>
2021-05-18 03:42:23 -0400

    react

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -3,14 +3,15 @@ import React from 'react';
 const Info = () => (
 	<div className="card">
 		<div className="card-image" style={{heigth: "150px"}}>
-          <span className="card-title">Welcome to Pester-bot!</span>
-        </div>
-        <div className="card-content">
-          <p>
-          This is a simple chatbot powered by Cleverbot API. You can asking it anything and it will do it's best to hold a conversion.
-          You can also tell the bot to change the message box color by type "set style A" or "set style B"
-          </p>
-        </div>
+      <img src="../assets/bot.jpg"/>
+      <span className="card-title">Welcome to Pester-bot!</span>
+    </div>
+    <div className="card-content">
+      <p>
+        This is a simple chatbot powered by Cleverbot API. You can asking it anything and it will do it's best to hold a conversion.
+        You can also tell the bot to change the message box color by type "set style A" or "set style B"
+      </p>
+    </div>
 	</div>
 );
 
